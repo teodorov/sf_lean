377ce064d75f3ac9fc4e395f435e5067f820f210
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2017-11-05 12:37:09 +0100

    method definition syntax example

--- smalltalk/smalltalk_ast_examples.lean
+++ smalltalk/smalltalk_ast_examples.lean
@@ -11,3 +11,43 @@ open statement
 #check assignment ("xxx")  (literal (static (symbol "xxx")))
 #check assignment "xxx" (literal (static (symbol "xxx")))
 
+--PCObject >> #asString -> [ ^ self printString ]
+#check 
+    send 
+        (literal (static (symbol "PCObject")))
+        {message .
+            selector := ">>",
+            arguments := 
+                [ send 
+                    (literal (static (symbol "asString")))
+                    { message .
+                        selector := "->",
+                        arguments := 
+                            [ block 
+                                [] 
+                                [] 
+                                [ return
+                                    ( send 
+                                        self 
+                                        { message .
+                                            selector := "printString",
+                                            arguments := []
+                                        }
+                                    )  
+                                ]
+                            ]
+                    }
+                ]
+        }
+
+
+-- PCObject >> #at: -> {
+-- 	[:index | VMProxy primitive: #at: with: { index } ].
+-- 	[:index |
+-- 		index isInteger
+-- 			ifTrue: [ self errorSubscriptBounds: index ].
+-- 		index isNumber
+-- 			ifTrue: [ ^ self basicAt: index asInteger ]
+-- 			ifFalse: [ self errorNonIntegerIndex ]
+-- 	]
+-- }
